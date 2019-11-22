ac647062d57baa5074f92f7a4e5c6a22c5f1ab4d
egehanhk <egehanhk@gmail.com>
2019-11-22 15:39:49 +0100

    Added party lists

---
+++ gcta/gcta_groups.js
@@ -0,0 +1,33 @@
+const party_lists = {
+    group_1: {
+        Trexnamedted: 1,
+        CrownMage: 1,
+        Stabara: 1,
+        Zorp: 1,
+        Zurp: 1,
+        Bjarny: 1,
+        Eyllis: 1,
+        Mufffin: 1,
+        Bjarni: 1
+    },
+    group_2: {
+        Trexnamedtut: 1,
+        CrownSpam: 1,
+        Aran: 1,
+        Zirp: 1,
+        Trexnamedtom: 1,
+        CrownsAnal: 1,
+        Nara: 1,
+        Bjarne: 1,
+        Shana: 1
+    }
+}
+
+let party_list = {};
+
+for (const group_name in party_lists) {
+    if (character.name in party_lists[group_name]) {
+        party_list = {...party_lists[group_name]};
+        break;
+    }
+}
