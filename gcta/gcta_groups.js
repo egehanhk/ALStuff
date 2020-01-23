6e1023be19c53cd2549f25f831544eb39f3ec878
trexnamedtom <blhartgrove@gmail.com>
2020-01-23 17:49:06 -0500

    changed names around
    me and crown wanted to farm together

--- gcta/gcta_groups.js
+++ gcta/gcta_groups.js
@@ -2,21 +2,21 @@ const party_lists = {
     group_1: {
         Trexnamedted: 1,
         CrownMage: 1,
-        Stabara: 1,
-        Zorp: 1,
-        Zurp: 1,
+        CrownSpam: 1,
+        Trexnamedtut: 1,
+        Trexnamedtom: 1,
+        CrownsAnal: 1,
+    },
+    group_2: {
+        //Stabara: 1,
+        //Zorp: 1,
+        //Zurp: 1,
         Bjarny: 1,
         Eyllis: 1,
         Mufffin: 1,
-        Bjarni: 1
-    },
-    group_2: {
-        Trexnamedtut: 1,
-        CrownSpam: 1,
+        Bjarni: 1,
         Aran: 1,
         Zirp: 1,
-        Trexnamedtom: 1,
-        CrownsAnal: 1,
         Nara: 1,
         Bjarne: 1,
         Shana: 1
